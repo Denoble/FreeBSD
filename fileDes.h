1cd54a2a171b8fd417328db47fae841c1804e4bd
Denoble <ujay2002@yahoo.com>
2015-12-02 08:49:52 -0330

    myfile

---
+++ fileDes.h
@@ -0,0 +1,9 @@
+#include<stdio.h>// standard function declared here
+#include<math.h>
+#include"time.h"
+#include<stdlib.h>
+#define DATAMAX 50
+ typedef struct{
+	 int fileDes;
+	 char data[DATAMAX];
+ }SocketMessage;
