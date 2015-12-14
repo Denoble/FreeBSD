36a5728395d735867c9d1688862d417c4d74f10b
Stanley Godfrey <denoble>
2015-12-14 09:02:29 -0330

    passfileDes.c

--- passfileDes.c
+++ passfileDes.c
@@ -13,40 +13,51 @@
 #include"fileDes.h"
 
 static int
-send_file_descriptor(
-  int socket, /* Socket through which the file descriptor is passed */
-  int fd_to_send) /* File descriptor to be passed, could be another socket */
+recv_file_descriptor(
+  int socket) /* Socket from which the file descriptor is read */
 {
+ int sent_fd;
  struct msghdr message;
  struct iovec iov[1];
  struct cmsghdr *control_message = NULL;
  char ctrl_buf[CMSG_SPACE(sizeof(int))];
  char data[1];
+ int res;
 
  memset(&message, 0, sizeof(struct msghdr));
  memset(ctrl_buf, 0, CMSG_SPACE(sizeof(int)));
 
- /* We are passing at least one byte of data so that recvmsg() will not return 0 */
- data[0] = ' ';
+ /* For the dummy data */
  iov[0].iov_base = data;
  iov[0].iov_len = sizeof(data);
 
  message.msg_name = NULL;
  message.msg_namelen = 0;
+ message.msg_control = ctrl_buf;
+ message.msg_controllen = CMSG_SPACE(sizeof(int));
  message.msg_iov = iov;
  message.msg_iovlen = 1;
- message.msg_controllen =  CMSG_SPACE(sizeof(int));
- message.msg_control = ctrl_buf;
 
- control_message = CMSG_FIRSTHDR(&message);
- control_message->cmsg_level = SOL_SOCKET;
- control_message->cmsg_type = SCM_RIGHTS;
- control_message->cmsg_len = CMSG_LEN(sizeof(int));
+ if((res = recvmsg(socket, &message, 0)) <= 0)
+  return res;
 
- *((int *) CMSG_DATA(control_message)) = fd_to_send;
+ /* Iterate through header to find if there is a file descriptor */
+ for(control_message = CMSG_FIRSTHDR(&message);
+     control_message != NULL;
+     control_message = CMSG_NXTHDR(&message,
+                                   control_message))
+ {
+  if( (control_message->cmsg_level == SOL_SOCKET) &&
+      (control_message->cmsg_type == SCM_RIGHTS) )
+  {
+   return *((int *) CMSG_DATA(control_message));
+  }
+ }
 
- return sendmsg(socket, &message, 0);
+ return -1;
 }
+
+
 
  int main(int argc,char* argv[]){
 
@@ -54,16 +65,16 @@ send_file_descriptor(
 	int len;
 	struct sockaddr_un address;
 	int result;
-	int a,b,c,d;
+	int i,b,c,d,n;
+	char readWords[128];
+	char uppercase[128];
+	char* newline="\n";
 	// Initallizing my Struct named SocketMessage
 	SocketMessage *sMsg= (SocketMessage *)malloc(sizeof(SocketMessage));
-	strcpy(sMsg->data,"Data from client received and printed \n");
-	sMsg->read_fileDes=open("Reading.txt",O_RDONLY);
-	if(sMsg->read_fileDes<0)
-			err(-1,"fail to open file Reading.txt");
-	sMsg->write_fileDes=open("Written.txt",O_WRONLY|O_APPEND|O_CREAT);
-	if(sMsg->write_fileDes<0)
-			err(-1,"fail to open file Writing.txt");
+	strcpy(sMsg->rdFileName,argv[2]);
+	strcpy(sMsg->rdRights,"CAP_READ");
+	strcpy(sMsg->wrFileName,argv[4]);
+	strcpy(sMsg->wrRights,"CAP_WRITE,CAP_CREATE");
 	
 	sockfd= socket(AF_UNIX,SOCK_STREAM,0);
 	address.sun_family=AF_UNIX;
@@ -74,29 +85,60 @@ send_file_descriptor(
 		perror("OOPS: Client cannot connect \n");
 		exit(1);
 	}
-	d=write(sockfd,sMsg->data,sizeof(sMsg->data));
-		if(d>0){
-			printf(" \n");
-			printf(" Data was sent to server successfully \n");
-			printf(" \n");
-			printf(" ...............................\n");
-			printf(" \n");
-		}
-		
-	c= send_file_descriptor(sockfd, sMsg->read_fileDes);
-		if(c<0)
-			err(-1,"cannot send file file descriptor a ");	
-	b= send_file_descriptor(sockfd, sMsg->write_fileDes);
-		if(b<0)
-			err(-1,"cannot send file file descriptor  b");	
+	cap_enter();
+	d=write(sockfd,sMsg->rdFileName,sizeof(sMsg->rdFileName));
+	c=write(sockfd,sMsg->rdRights,sizeof(sMsg->rdRights));
+	
+	
+	
+	
+	if(d>0 && c>0){
+	  printf(" \n");
+	  printf(" file name %s was sent to server successfully \n",sMsg->rdFileName);
+	  printf(" \n");
+	  printf(" ...............................\n");
+	  printf(" \n");
+	}
+	sMsg->fileDesRd=recv_file_descriptor(sockfd);
+	if(sMsg->fileDesRd<0)
+			err(-1,"can't read from socket");
+	
+	d=write(sockfd,sMsg->wrFileName,sizeof(sMsg->wrFileName));
+	c=write(sockfd,sMsg->wrRights,sizeof(sMsg->wrRights));
+	
+	
+	if(d>0 && c>0){
+	  printf(" \n");
+	  printf("file name %s  was sent to server successfully \n",sMsg->wrFileName);
+	  printf(" \n");
+	  printf(" ...............................\n");
+	  printf(" \n");
+	}
+	sMsg->fileDesWr=recv_file_descriptor(sockfd);
+	if(sMsg->fileDesWr<0)
+			err(-1,"can't read from socket");
 	
 
+	do{
+		n=read(sMsg->fileDesRd,readWords,sizeof(readWords));
+		if(n>0){
+			for(i=0;i<n;i++){
+				uppercase[i]=toupper(readWords[i]);
+			}
+			write(sMsg->fileDesWr,uppercase,n);
+			write(sMsg->fileDesWr,newline,strlen(newline));
+			}				
+				
+			
+	}while(n>0);
+
+	write(sMsg->fileDesWr,newline,strlen(newline));
+		
 	
-	close(sMsg->read_fileDes);
-	close(sMsg->write_fileDes);
+	close(sMsg->fileDesRd);
+	close(sMsg->fileDesWr);
 	close(sockfd);
 	
 	free(sMsg);
 	return 0;	
 }
-
