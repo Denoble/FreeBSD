a8a65d92920397a7f3fb0aefe26fe499374a3dfe
Stanley Godfrey <denoble>
2015-12-14 09:12:55 -0330

    fileDes.h

--- fileDes.h
+++ fileDes.h
@@ -2,7 +2,11 @@
 #include<stdlib.h>
 #define DATAMAX 50
  typedef struct{
-	 int read_fileDes;
-	 int write_fileDes;
+	 int fileDesRd;
+	 int fileDesWr;
 	 char data[DATAMAX];
+	char rdFileName[DATAMAX];
+	char wrFileName[DATAMAX];
+	char rdRights[DATAMAX];
+	char wrRights[DATAMAX];
  }SocketMessage;
